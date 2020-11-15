85319a77027c7708d056a9087edac8bb9f75786f
Borya <37692980+sescer@users.noreply.github.com>
2020-11-15 18:23:33 +0700

    Update Software.asm

--- Software.asm
+++ Software.asm
@@ -3,54 +3,54 @@ asect 0
 main:
 	ldi r2,3	
 	ld r2,r1
-	while # ждём нажатия кнопки
+	while # waiting for the button to be pressed
 		tst r1
 	stays mi
 		ld r2,r1
 	wend
 	ld r1,r0
-	if # проверяем, не занята ли клетка
+	if # check if the cell is busy
 		tst r0
 	is nz
 		br main
 	fi
 	ldi r0,2
-	st r1,r0 # сохраняем крестик по адресу нажатой клетки
+	st r1,r0 # save the cross at the address of the pressed cell
 	cmp r1,r1
-	shla r1 # формируем запрос о том, что крестик нажат
-	shla r1 # по присланному адресу
+	shla r1 # we form a request that the cross is pressed
+	shla r1 # at the sent address
 	add r0,r1
-	st r2,r1 # отправляем запрос
+	st r2,r1 # send a request
 	jsr check_game_state
 	tst r0
-	bnz end_game # если результат ненулевой, значит игра закончилась 
+	bnz end_game # if the result is nonzero, then the game is over
 	jsr computer_turn
 	ldi r1,1
-	st r0,r1 # сохраняем нолик по адресу в память
+	st r0,r1 # save zero to address in memory
 	cmp r0,r0
 	shla r0
 	shla r0
 	add r0,r1
-	st r2,r1 # отправляем запрос 
+	st r2,r1 # send a request
 	jsr check_game_state
 	tst r0
-	bnz end_game # если результат ненулевой, значит игра закончилась 
+	bnz end_game # if the result is nonzero, then the game is over
 	br main
 end_game:
 	ldi r1,3
 	shr r0
 	shr r0
 	shr r0
-	ldi r2, # 60 единицы в 2-5 битах - специальное значение для LLL
+	ldi r2, # 60 ones in 2-5 bits - special value for LLL
 	or r2,r0
-	st r1,r0 # отправляем запрос
+	st r1,r0 # send a request
 	halt
 computer_turn:
 	push r1
 	push r2
 	push r3
 	ldi r1,7
-	ld r1,r0 # считываем данные с адреса 7
+	ld r1,r0 # read data from address 7
 	br end_func
 check_game_state:
 	push r1
@@ -58,7 +58,7 @@ check_game_state:
 	push r3
 	ldi r1,3
 	ld r1,r0
-	shra r0 # оставляем только информацию о состоянии доски
+	shra r0 # leave only information about the state of the board
 	shra r0
 	shra r0
 	shra r0
